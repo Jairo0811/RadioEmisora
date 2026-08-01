f8c4677502ede7fcb1a2ef44d583ce542915dc93
Jairo Matías <jairo_1829@hotmail.com>
2026-08-01 13:56:52 -0400

    Add explicit HTTP namespaces for WPF markup builds

--- RadioEmisoraRD/RadioEmisoraRD.Tests/CatalogAndSearchTests.cs
+++ RadioEmisoraRD/RadioEmisoraRD.Tests/CatalogAndSearchTests.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using System.Text.Json;
 using System.Windows.Media;
 using RadioEmisoraRD.Models;
--- RadioEmisoraRD/RadioEmisoraRD/Services/MediaPlayerService.cs
+++ RadioEmisoraRD/RadioEmisoraRD/Services/MediaPlayerService.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using RadioEmisoraRD.Models;
 
 namespace RadioEmisoraRD.Services;
